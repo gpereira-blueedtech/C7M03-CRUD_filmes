08b4901bdd9690b863cbf6ce2c6782761af9bb45
Gabriel Lima <gpereira@blueedtech.com.br>
2021-10-19 20:21:11 -0300

    Comentado index database

--- database/index.js
+++ database/index.js
@@ -1,17 +1,16 @@
 // Aqui é onde farei a conexão ao meu banco de dados.
 
-// const Sequelize = require("sequelize");
+const Sequelize = require("sequelize");
 
+// Aqui é feita a conexão ao meu banco local
 // const sequelize = new Sequelize(process.env.DB_BASE, process.env.DB_USER,process.env.DB_PASS, {
 //   host:  process.env.DB_HOST,
 //   port: process.env.DB_PORT,
 //   dialect: 'postgres'
 // });
 
-// module.exports = sequelize;
 
-const Sequelize = require("sequelize");
-
+// Aqui faço a conexão com o banco remoto - Heroku
 const sequelize = new Sequelize(process.env.DATABASE_URL, {     
   dialect: 'postgres',
   protocol: 'postgres',     
